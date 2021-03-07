435358e69a84a9b7bdbd6ebd68f3e1c818da1709
its-rishav <pandey.rishav@tftus.com>
2021-03-08 03:42:37 +0530

    adding all the services through which data for viewnetwork service is evaluated

--- server.go
+++ server.go
@@ -15,6 +15,18 @@ type userserver struct {
 	pb.UnimplementedUserServiceServer
 }
 
+type contactserver struct {
+	pb.UnimplementedContactServiceServer
+}
+
+type interestserver struct {
+	pb.UnimplementedInterestsServiceServer
+}
+
+type viewserver struct {
+	pb.UnimplementedViewNetworkServiceServer
+}
+
 type networkData struct {
 	nKey  int64
 	nName string
@@ -22,8 +34,10 @@ type networkData struct {
 }
 
 type userData struct {
-	key  int64
-	name string
+	key       int64
+	name      string
+	contacts  []string
+	interests []string
 }
 
 func getNetworkData() []networkData {
@@ -49,6 +63,16 @@ func getNetworkData() []networkData {
 	return networkData
 }
 
+func getUserData() []userData {
+	userData := []userData{
+		{key: 3, name: "rishav", contacts: []string{"saurabh", "rajesh", "surabhi"}, interests: []string{"table tennis", "basketball"}},
+		{key: 6, name: "saurabh", contacts: []string{"rishav", "rajesh", "surabhi"}, interests: []string{"table tennis", "football"}},
+		{key: 7, name: "vijay", contacts: []string{"rishav"}, interests: []string{"football"}},
+		{key: 8, name: "rajesh", contacts: []string{"saurabh", "rishav"}, interests: []string{"table tennis", "basketball", "football"}},
+	}
+	return userData
+}
+
 const (
 	port = ":50051"
 )
@@ -63,6 +87,66 @@ func (s *server) GetNetworkMembers(ctx context.Context, networkkey *pb.NetworkKe
 	}
 	return nil, fmt.Errorf("No Network found for key: %v", networkkey.Key)
 	// return &pb.UserKeys{Users: make(map[]*pb.UserKey, )}, nil
+}
+
+func (s *userserver) GetUser(ctx context.Context, ukey *pb.UserKey) (*pb.User, error) {
+	fmt.Println("User key received: ", ukey.Key)
+	userData := getUserData()
+	for i := range userData {
+		if userData[i].key == ukey.Key {
+			return &pb.User{Key: userData[i].key, Name: userData[i].name}, nil
+		}
+	}
+	return nil, fmt.Errorf("No Network found for key: %v", ukey.Key)
+
+}
+
+func (s *contactserver) GetCommonContacts(ctx context.Context, uKeys *pb.TwoUserKeys) (*pb.Contacts, error) {
+	fmt.Printf("Recieved user keys user1: %v, user2: %v\n", uKeys.User1.Key, uKeys.User2.Key)
+	user1 := uKeys.GetUser1()
+	user2 := uKeys.User2
+	userData := getUserData()
+	u1Contacts, u2Contacts := []string{}, []string{}
+	commonContacts := []string{}
+	for i := range userData {
+		if userData[i].key == user1.Key {
+			u1Contacts = append(u1Contacts, userData[i].contacts...)
+		} else if userData[i].key == user2.Key {
+			u2Contacts = append(u2Contacts, userData[i].contacts...)
+		}
+	}
+	for u1i := range u1Contacts {
+		for u2i := range u2Contacts {
+			if u1Contacts[u1i] == u2Contacts[u2i] {
+				commonContacts = append(commonContacts, u1Contacts[u1i])
+			}
+		}
+	}
+	return &pb.Contacts{Contacts: commonContacts}, nil
+}
+
+func (s *interestserver) GetSharedInterests(ctx context.Context, uKeys *pb.TwoUserKeys) (*pb.Interests, error) {
+	fmt.Printf("Recieved user keys user1: %v, user2: %v\n", uKeys.User1.Key, uKeys.User2.Key)
+	user1 := uKeys.GetUser1()
+	user2 := uKeys.User2
+	userData := getUserData()
+	u1Interests, u2Interests := []string{}, []string{}
+	commonInterests := []string{}
+	for i := range userData {
+		if userData[i].key == user1.Key {
+			u1Interests = append(u1Interests, userData[i].interests...)
+		} else if userData[i].key == user2.Key {
+			u2Interests = append(u2Interests, userData[i].interests...)
+		}
+	}
+	for u1i := range u1Interests {
+		for u2i := range u2Interests {
+			if u1Interests[u1i] == u2Interests[u2i] {
+				commonInterests = append(commonInterests, u1Interests[u1i])
+			}
+		}
+	}
+	return &pb.Interests{Interests: commonInterests}, nil
 }
 
 // func main() {
--- server_test.go
+++ server_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestGetNetworkMembers(t *testing.T) {
-	s := Server{}
+	s := server{}
 	ctx := context.Background()
 	key := &pb.NetworkKey{Key: 12}
 	response, err := s.GetNetworkMembers(ctx, key)
@@ -17,12 +17,54 @@ func TestGetNetworkMembers(t *testing.T) {
 	}
 	userCount := len(response.Users)
 
-	if userCount != 2 {
+	if userCount != 3 {
 		t.Errorf("expected: 2, got %v", userCount)
 	}
-	// userCount := len(response.users)
-	// if userCount != 2 {
-	// 	t.Error("Expected 2, got ", userCount)
-	// }
 
+}
+
+func TestGetUser(t *testing.T) {
+	userver := userserver{}
+	ctx := context.Background()
+	key := &pb.UserKey{Key: 6}
+	response, err := userver.GetUser(ctx, key)
+	if err != nil {
+		fmt.Errorf("err %v", key)
+	}
+	fmt.Println(response)
+	if response.Name != "saurabh" {
+		t.Errorf("expected: saurabh, got %v", response.Name)
+	}
+}
+
+func TestGetCommonContacts(t *testing.T) {
+	contactserver := contactserver{}
+	ctx := context.Background()
+	user1 := &pb.UserKey{Key: 3}
+	user2 := &pb.UserKey{Key: 6}
+	twoUsers := &pb.TwoUserKeys{User1: user1, User2: user2}
+	response, err := contactserver.GetCommonContacts(ctx, twoUsers)
+	if err != nil {
+		fmt.Errorf("err %v", twoUsers)
+	}
+	commonContacts := len(response.Contacts)
+	if commonContacts != 2 {
+		t.Errorf("expected commoncontacts: 2, got %v", commonContacts)
+	}
+}
+
+func TestGetSharedInterests(t *testing.T) {
+	s := interestserver{}
+	ctx := context.Background()
+	user1 := &pb.UserKey{Key: 7}
+	user2 := &pb.UserKey{Key: 6}
+	twoUsers := &pb.TwoUserKeys{User1: user1, User2: user2}
+	response, err := s.GetSharedInterests(ctx, twoUsers)
+	if err != nil {
+		fmt.Errorf("err %v", twoUsers)
+	}
+	commonInterests := len(response.Interests)
+	if commonInterests != 1 {
+		t.Errorf("expected commonInterests: 2, got %v", commonInterests)
+	}
 }
